fff0c9fd8cf6fa50284c68ac6252b50beb2fd51f
iker soler <ikersonic@gmail.com>
2024-10-14 10:33:37 +0200

    scripts empezados

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool softTransition = false;
+    public float transitionVelocity = 10f;
+    public float rotationTransitionVelocity = 500f;
+
+
+
+
+    Vector3 targetGridPos;
+    Vector3 prevTargetGridPos;
+    Vector3 targetRotation;
+
+    private void Start()
+    {
+        targetGridPos = Vector3Int.RoundToInt(transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (true)
+        {
+
+            prevTargetGridPos = targetGridPos;
+
+            Vector3 targetPosition = targetGridPos;
+
+            if (targetRotation.y > 270f && targetRotation.y < 361f) targetRotation.y = 0f;
+            if (targetRotation.y < 0f) targetRotation.y = 270f;
+
+           // if (!smoothTransition)
+           // {
+
+            //}
+        }else{
+
+            targetGridPos = prevTargetGridPos;
+
+        }
+    }
+
+    public void RotateLeft() { if (AtRest) targetRotation -= Vector3.up * 90f; }
+    public void RotateRight() { if (AtRest) targetRotation += Vector3.up * 90f; }
+    public void MoveForeward() { if (AtRest) targetGridPos += transform.forward; }
+    public void MoveBack() { if (AtRest) targetGridPos -= transform.forward; }
+    public void MoveLeft() { if (AtRest) targetGridPos -= transform.right; }
+    public void MoveRight() { if (AtRest) targetGridPos += transform.right; }
+
+
+
+
+
+
+
+    bool AtRest
+    {
+        get
+        {
+            if((Vector3.Distance(transform.position, targetGridPos) < 0.05f) &&
+               (Vector3.Distance(transform.eulerAngles, targetRotation) < 0.05f))
+                return true;
+            else
+                return false;
+        }
+    }
+}
