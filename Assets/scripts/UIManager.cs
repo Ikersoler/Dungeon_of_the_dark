608cebbb4165307fca3ea454f4f9dda9dee9b85c
iker soler <ikersonic@gmail.com>
2025-02-17 11:23:50 +0100

    preparar menus

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -1,8 +1,65 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 public class UIManager : MonoBehaviour
 {
     public CombatSystem combatSystem;
+
+    public GameObject pausePanel;
+
+
+    private void Start()
+    {
+        HidePause();
+        
+    }
+
+    private void Update()
+    {
+        //con esto oculto y muestro el inventario
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowPause();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            HidePause();
+        }
+   
+    }
+
+
+    private void ShowPause()
+    {
+      combatSystem.combatUI.SetActive(false);
+      combatSystem.basicUI.SetActive(false);
+      pausePanel.SetActive(true);
+    }
+
+
+    private void HidePause()
+    {
+        combatSystem.combatUI.SetActive(false);
+        combatSystem.basicUI.SetActive(false);
+        pausePanel.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void OnAttackButton()
     {
